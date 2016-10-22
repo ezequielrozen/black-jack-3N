9832ba922341b1d9333acc7e920b8fb264330743
vagrant <vagrant@kleer-csd-box>
2016-10-22 14:22:32 +0200

    plantase feature

--- spec/jugador_spec.rb
+++ spec/jugador_spec.rb
@@ -7,4 +7,14 @@ describe "Jugador" do
 		partida.iniciarPartida
 		expect(partida.getJugador.estaPlantado).to eq false
 	end
+
+	it "El jugador se planta" do
+		partida = Partida.new
+		partida.iniciarPartida
+		jugador = partida.getJugador
+		jugador.plantarse
+		expect(jugador.estaPlantado).to eq true
+	end
+
+
 end
